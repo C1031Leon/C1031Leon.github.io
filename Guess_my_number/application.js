eba1590bdf67dead51860a2c36906124102aca5e
C1031Leon <CLeon@kippnyccp.org>
2015-04-14 13:16:46 -0400

    Update application.js

--- Guess_my_number/application.js
+++ Guess_my_number/application.js
@@ -1,24 +1,51 @@
 $(document).ready(function(){
   //find a way for the program to choose a random number between 1 and 100, save this as a variable
-  
-  
-  //when the player clicks on the 'guess' button
-  $('button').on('click', function(){
-    //save their guess as a variable
-    
-    
-    //and compare this guess to the random number that the computer picked
-    //if the user guessed the correct number...
-    
-      //what happens if the guess is correct?
-      
-    //if the user guessed too high...
-    
-      //update the 'feedback' paragraph to tell them to guess lower
-      
-    //otherwise, the user guessed too low...
-    
-      //update the 'feedback' paragraph to tell them to guess higher
-    
-  });
-});
+            var game = {
+                num : 0,
+                turns : 1,
+                reset : function() {
+                    this.turns = 1;
+                    this.newNum();
+                },
+                newNum() : function() {
+                    this.num = parseInt(Math.random() * 10) +1;
+                },
+                checkNum() : function(guess) {
+                    try {
+                        guess = parseInt(guess);
+                    }
+                    catch(e) {
+                        alert("Enter a guess!");
+                        this.turns++;
+                        return false;
+                    }
+
+                    if (guess == this.num) {
+                        alert("Correct! It took you " + this.turns + "turns to guess my number.");
+                        return true;
+                    }
+                    else if(guess > this.num) {
+                        alert("Your guess is too high. Try again.");
+                        this.turns++;
+                        return false;
+                    }
+                    else (guess < this.num) {
+                        alert("Your guess is too low. Try again.");
+                        this.turns++;
+                        return false;
+                    }
+                }
+            };
+
+            function guessNumber() {
+                var guess = document.getElementById("guess").value;
+                game.checkGuess(guess);
+            }
+
+            function resetGame() {
+                game.reset();
+            }
+
+            resetGame(); 
+)};
+
